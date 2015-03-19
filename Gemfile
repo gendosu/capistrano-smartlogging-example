202ce56041e948a3a56f2921cfb911f22eb520be
Gen Takahashi <gendosu@gmail.com>
2015-03-19 16:25:25 +0900

    add source

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem "capistrano"
+
+gem 'capistrano-smartlogging'
+
+# for debug
+#gem 'capistrano-smartlogging', :github => 'gendosu/capistrano-smartlogging'
+
+# for debug
+#gemspec path: "../capistrano-smartlogging"
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'cap_t1', user: 'root', roles: %w{app}
+server 'localhost', user: 'root', roles: %w{app}
 
 
 # role-based syntax
